fb4aae595d1cf2237cc758e21e86d42492e81cc2
mmyali26 <mmc@oc5485748714.ibm.com>
2019-06-28 14:07:44 +0530

    more code samples added

---
+++ worker.py
@@ -0,0 +1,33 @@
+import asyncio
+import random
+from colorama import Fore
+
+colors = [
+    Fore.BLUE,
+    Fore.CYAN,
+    Fore.GREEN,
+    Fore.MAGENTA,
+    Fore.RED,
+    Fore.WHITE,
+    Fore.YELLOW
+]
+
+
+async def worker(cid, wname):
+    ctr = 0
+    stime = random.randint(1,5) / 0.7
+    while True:
+        await asyncio.sleep(stime)
+        ctr += 1
+        print(f"{cid} Worker {wname} processing [{ctr}]")
+
+
+async def main():
+    print("Starting workers.....!")
+    loop = asyncio.get_event_loop()
+    tasks = [asyncio.create_task(worker(random.sample(colors, 1)[0], f"Name {x}")) for x in range(10)]
+    await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
+
